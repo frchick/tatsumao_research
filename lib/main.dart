ebd5600be72772a890373014d4eb5705ee930f80
frchick <frchick.00@gmail.com>
2022-11-22 01:32:10 +0900

    一部タイルを assets から読み込む実験

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:io'; // HttpClient
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart';
+import 'package:http/retry.dart';
+
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map/src/layer/tile_layer/tile_provider/network_image_provider.dart';
+import 'package:flutter_map/src/layer/tile_layer/tile_provider/network_no_retry_image_provider.dart'; // FMNetworkNoRetryImageProvider
 
 void main()
 {
@@ -52,6 +60,7 @@ class _MyHomePageState extends State<MyHomePage>
             // 高さ陰影図
             TileLayerOptions(
               urlTemplate: "https://cyberjapandata.gsi.go.jp/xyz/hillshademap/{z}/{x}/{y}.png",
+              tileProvider: MyTileProvider(headers: {'User-Agent': 'flutter_map (unknown)'}),
             ),
             // 標準地図
             TileLayerOptions(
@@ -64,3 +73,58 @@ class _MyHomePageState extends State<MyHomePage>
     );
   }
 }
+
+// NetworkNoRetryTileProvider のカスタム(WEB専用)
+class MyTileProvider extends TileProvider {
+  MyTileProvider({
+    Map<String, String>? headers,
+  }) {
+    this.headers = headers ?? {};
+  }
+
+  @override
+  ImageProvider getImage(Coords<num> coords, TileLayerOptions options)
+  {
+    // options.zoomReverse, options.zoomOffset は使わない前提で無視
+    var zoom = coords.z.round();
+    var x = coords.x.round();
+    var y = coords.y.round();
+
+    if((zoom == 16) && (x == 58085) && ((25888 <= y) && (y <= 25890))){
+      String path = "map_tiles/xyz/hillshademap/${zoom}/${x}_${y}.png";
+      print("path=${path} ****");
+      return AssetImage(path);
+    }
+
+    final String url = getTileUrl(coords, options);
+    print("url=${url}");
+    return NetworkImage(
+      url,
+      headers: headers..remove('User-Agent'),
+    );
+  }
+}
+
+/* NetworkNoRetryTileProvider のカスタム(WEB以外)
+class MyTileProvider extends TileProvider
+{
+  MyTileProvider({
+    Map<String, String>? headers,
+    HttpClient? httpClient,
+  }) {
+    this.headers = headers ?? {};
+    this.httpClient = httpClient ?? HttpClient()
+      ..userAgent = null;
+  }
+
+  late final HttpClient httpClient;
+
+  @override
+  ImageProvider getImage(Coords<num> coords, TileLayerOptions options) =>
+      FMNetworkNoRetryImageProvider(
+        getTileUrl(coords, options),
+        headers: headers,
+        httpClient: httpClient,
+      );
+}
+*/
